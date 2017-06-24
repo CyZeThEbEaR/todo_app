4340d9d9bd9a5b2852d5345e351c5b2759410fbc
Nicholas Cyze <nickcyze@gmail.com>
2017-06-24 19:07:16 +0000

    Alot

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,43 @@
 class TodoController < ApplicationController
-  def index
-  end
-    
-  def show
-      @pomodoro_estimate=7
-      @pomodoro_description="Start the day of great"
-  end
+    def index
+    end
+     
+    def show
+        todo_id=params[:id]
+        if todo_id == '1'
+            @pomodoro_title= "Wake up"
+            @pomodoro_estimate=4
+            @pomodoro_description="Start the day of great"
+        end
+        if todo_id=='2'
+            @pomodoro_title= "Get ready for the Day"
+            @pomodoro_estimate=3
+            @pomodoro_description="Put some clothes on"
+        end
+        if todo_id=='3'
+            @pomodoro_title="Check on Summer Work"
+            @pomodoro_estimate=2
+            @pomodoro_description="Do the Work for School"
+        end
+        if todo_id=='4'
+            @pomodoro_title="Eat food"
+            @pomodoro_estimate=1
+            @pomodoro_description="Eat healthy"
+        end
+        if todo_id=='5'
+            @pomodoro_title="Work Out"
+            @pomodoro_estimate=2
+            @pomodoro_description="Pump some Iron"
+        end
+        if todo_id=='6'
+            @pomodoro_title="Hang with Friends"
+            @pomodoro_estimate=1
+            @pomodoro_description="Go out and have some fun"
+        end
+        if todo_id=='7'
+            @pomodoro_title="Sleep"
+            @pomodoro_estimate=0
+            @pomodoro_description="Go back to bed"
+        end
+    end
 end
